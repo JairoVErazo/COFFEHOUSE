4da1964a6cd143635f5214dd6e2564b9fa82a2af
LilianaAguilar14 <92348509+LilianaAguilar14@users.noreply.github.com>
2024-04-24 09:28:08 -0600

    navbar

--- COFFEHOUSE.Server/COFFEHOUSE/src/app/layout.jsx
+++ COFFEHOUSE.Server/COFFEHOUSE/src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{ backgroundImage: 'url("img/fondo.png")', backgroundSize: 'cover', minHeight: '100vh' }}>
         <NavBar />
         {children}
       </body>
--- COFFEHOUSE.Server/COFFEHOUSE/src/app/page.jsx
+++ COFFEHOUSE.Server/COFFEHOUSE/src/app/page.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <h1>helloo</h1>
+      
+    </div>
+  );
 }
